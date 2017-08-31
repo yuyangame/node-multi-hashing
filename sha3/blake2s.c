c0c14c7509a9e75dc0ae763a6a48ff567ae78698
Kvial <kangzhq@foxmail.com>
2017-09-01 00:58:32 +0800

    a

--- sha3/blake2s.c
+++ sha3/blake2s.c
@@ -120,8 +120,8 @@ static inline int blake2s_param_set_personal( blake2s_param *P, const uint8_t pe
 static inline int blake2s_init0( blake2s_state *S )
 {
 	memset( S, 0, sizeof( blake2s_state ) );
-
-	for( int i = 0; i < 8; ++i ) S->h[i] = blake2s_IV[i];
+	int i;
+	for( i = 0; i < 8; ++i ) S->h[i] = blake2s_IV[i];
 
 	return 0;
 }
@@ -133,7 +133,8 @@ int blake2s_init_param( blake2s_state *S, const blake2s_param *P )
 	uint32_t *p = ( uint32_t * )( P );
 
 	/* IV XOR ParamBlock */
-	for( size_t i = 0; i < 8; ++i )
+	size_t i;
+	for( i = 0; i < 8; ++i )
 		S->h[i] ^= load32( &p[i] );
 
 	return 0;
@@ -198,11 +199,11 @@ int blake2s_compress( blake2s_state *S, const uint8_t block[BLAKE2S_BLOCKBYTES] 
 {
 	uint32_t m[16];
 	uint32_t v[16];
-
-	for( size_t i = 0; i < 16; ++i )
+	size_t i;
+	for( i = 0; = 0; i < 16; ++i )
 		m[i] = load32( block + i * sizeof( m[i] ) );
-
-	for( size_t i = 0; i < 8; ++i )
+	size_t i;
+	for( i = 0;= 0; i < 8; ++i )
 		v[i] = S->h[i];
 
 	v[ 8] = blake2s_IV[0];
@@ -245,8 +246,8 @@ int blake2s_compress( blake2s_state *S, const uint8_t block[BLAKE2S_BLOCKBYTES] 
 	ROUND( 7 );
 	ROUND( 8 );
 	ROUND( 9 );
-
-	for( size_t i = 0; i < 8; ++i )
+	size_t i;
+	for( i = 0;; i < 8; ++i )
 		S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
 
 #undef G
@@ -301,8 +302,8 @@ int blake2s_final( blake2s_state *S, uint8_t *out, uint8_t outlen )
 	blake2s_set_lastblock( S );
 	memset( S->buf + S->buflen, 0, 2 * BLAKE2S_BLOCKBYTES - S->buflen ); /* Padding */
 	blake2s_compress( S, S->buf );
-
-	for( int i = 0; i < 8; ++i ) /* Output full hash to temp buffer */
+	int i;
+	for( i = 0; i < 8; ++i ) /* Output full hash to temp buffer */
 		store32( buffer + sizeof( S->h[i] ) * i, S->h[i] );
 
 	memcpy( out, buffer, outlen );
@@ -341,14 +342,14 @@ int main( int argc, char **argv )
 {
 	uint8_t key[BLAKE2S_KEYBYTES];
 	uint8_t buf[KAT_LENGTH];
-
-	for( size_t i = 0; i < BLAKE2S_KEYBYTES; ++i )
+	size_t i;
+	for( i = 0 = 0; i < BLAKE2S_KEYBYTES; ++i )
 		key[i] = ( uint8_t )i;
-
-	for( size_t i = 0; i < KAT_LENGTH; ++i )
+	size_t i;
+	for( i = 0; i < KAT_LENGTH; ++i )
 		buf[i] = ( uint8_t )i;
-
-	for( size_t i = 0; i < KAT_LENGTH; ++i )
+	size_t i;
+	for( i = 0; i < KAT_LENGTH; ++i )
 	{
 		uint8_t hash[BLAKE2S_OUTBYTES];
 		blake2s( hash, buf, key, BLAKE2S_OUTBYTES, i, BLAKE2S_KEYBYTES );
